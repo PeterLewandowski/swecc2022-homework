86aea26d9fca09d846a21313c77946c1c34038d2
Piotr Lewandowski <piotrl@Piotrs-MacBook-Pro.local>
2022-01-19 14:36:11 -0500

    first git commit

---
+++ function.js
@@ -0,0 +1,32 @@
+function addNum(num1, num2){
+    return(num1 + num2)
+}
+
+let a = 30
+
+let b = 1
+
+console.log(`Result is ${addNum(30, 1)}`)
+
+
+//Rueben
+function addition(a, b,){
+    return a+b
+}
+console.log(addition(2,4))
+
+
+//Todd
+function addition(a, b,){
+    return a+b
+}
+
+console.log("4+7=", addition(4,7))
+
+const herAge = 30
+const hisAge = 31
+
+const totalAge = addition(30,31)
+
+console.log("The total of Danielle and Jeremiah;'s ages are", totalAge)
+//...rest of code is in GeniusScan on phone...
---
+++ variable.js
@@ -0,0 +1,26 @@
+let country = 'Poland'
+
+let continent = 'Europe'
+
+let population = 38_000_000
+
+console.log('Country:', country)
+
+console.log('Continent:', continent)
+
+console.log('Population:', population)
+
+let city = 'Warsaw'
+
+let areaCode = '5643'
+
+console.log('Stats:', {country, continent, population})
+
+birthDay = 2
+
+console.log(birthDay)
+
+
+
+
+
